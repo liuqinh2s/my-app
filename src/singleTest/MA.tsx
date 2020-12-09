3d13fca9363f86eccef28f8393714e244aecd9a3
liuqin <liuqinh2s@gmail.com>
2020-12-09 17:05:43 +0800

    create app

---
+++ src/singleTest/MA.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function MA() {
+  return (
+    <div className="MA">
+      <input></input>
+      <button></button>
+      <span></span>
+    </div>
+  );
+}
+
+export default MA;
